1916d74026d42e2fe12b8f01d2943e524fb4964f
rylda <71103413+rylda@users.noreply.github.com>
2021-03-14 13:03:27 -0500

    help

--- finalsite2/homepage/script.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementsByClassName ('toggle-button') [0]
-const topnavLinks = document.getElementsByClassName('topnav-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    
-    navbarLinks.classList.toggle('active')
-    
-})
